859f884ac8d63543df901747580ad8959428c10e
Ioannis Tambouras <ioannis@cpan.org>
2011-08-28 11:58:26 +0000

    Import of IOANNIS/PerlIO-via-Skip-0.06 from CPAN.
    gitpan-cpan-distribution: PerlIO-via-Skip
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         IOANNIS/PerlIO-via-Skip-0.06.tar.gz
    gitpan-cpan-author:       IOANNIS
    gitpan-cpan-maturity:     released

--- lib/PerlIO/via/Skip.pm
+++ lib/PerlIO/via/Skip.pm
@@ -7,7 +7,7 @@ use warnings;
 
 require Exporter;
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 use constant DEFAULTS  => ( start          =>  undef  , 
                             end            =>  undef  , 
 			    maxlines       =>  undef  ,
